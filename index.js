45b2b0d16d32a53dcbe51d5478374caa4ec7c84b
12jyo <jyotiss1312@gmail.com>
2021-07-13 12:59:11 -0500

    Arithmetic op route added

--- index.js
+++ index.js
@@ -13,5 +13,23 @@ app.get("/add/:no1/:no2", (req, res) => {
     })
 })
 
+app.get("/sub/:no1/:no2", (req, res) => {
+    res.json({
+        subtraction: (+req.params.no1) - (+req.params.no2)
+    })
+})
+
+app.get("/mul/:no1/:no2", (req, res) => {
+    res.json({
+        multiplication: (+req.params.no1) * (+req.params.no2)
+    })
+})
+
+app.get("/div/:no1/:no2", (req, res) => {
+    res.json({
+        division: (+req.params.no1) / (+req.params.no2)
+    })
+})
+
 app.listen(3000);
 console.log("Hello World");
